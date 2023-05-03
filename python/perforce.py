4c7a1f9136f1b8c22f901ea2cfaa1ecddb828a44
Theo Keeler <theo.keeler@inflexion.io>
2023-05-03 10:54:03 -0600

    fix: Fix missing tuple in log

--- python/perforce.py
+++ python/perforce.py
@@ -138,7 +138,7 @@ class P4Repo:
         # Set root, stream, and view from supplied values. Detect if we need to run `p4 switch`.
         if self.stream and client["Stream"] != self.stream:
             try:
-                self.perforce.logger.info("p4config last stream was %s, switching to %s" % client["Stream"] self.stream)
+                self.perforce.logger.info("p4client last stream was %s, switching to %s" % (client["Stream"], self.stream))
                 self.perforce.run_switch('-Rn', "--no-sync", self.stream)
                 
                 client = self.perforce.fetch_client(clientname)
